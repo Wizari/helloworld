30776789690e1379faa282723a31b210cadd36e9
Ivan Baranov <3903672@gmail.com>
2018-11-06 21:43:12 +0300

    Initial

---
+++ src/main/java/com/gmail/danze/Application.java
@@ -0,0 +1,14 @@
+package com.gmail.danze;
+
+public class Application {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 5};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        int x = 3;
+        int y = 2;
+        int result = x + y;
+        int i = 4 + 4;
+    }
+}
